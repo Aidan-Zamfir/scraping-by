876dfd3615e333dffc9ff72914199a1bc7dbd778
Aidan-Z <aidanzamfir@gmail.com>
2024-06-08 20:07:06 -0300

    scraper struct

--- scraper/webscraper.go
+++ scraper/webscraper.go
@@ -1,1 +1,22 @@
 package scraper
+
+import (
+	"github.com/gocolly/colly"
+)
+
+type Rental struct {
+	Neighborhood string `json:"neighborhood"`
+	Price        string `json:"price"`
+	PriceSQM     string `json:"price_sqm"`
+	Kitchens     string `json:"kitchens"`
+	LivingR      string `json:"living_rooms"`
+	Bathrooms    string `json:"bathrooms"`
+	LastUpdated  string `json:"last_updated"`
+	PropertyCode string `json:"property_code"`
+	Link         string `json:"link"`
+}
+
+func ScrapeData() {
+	c := colly.NewCollector()
+
+}
